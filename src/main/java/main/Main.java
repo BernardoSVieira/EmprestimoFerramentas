1dc1b2d1f2e2f042f8de14dcc7f0891e06d4c120
GuilhermeSchveitzer <>
2024-05-28 11:11:06 -0300

    Docs: Main e pom

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,22 @@
 
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    
-    //Teste
-    //teste
-    //teste 
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/EmprestimoFerramenta";
+        String user = "root";
+        String password = "root";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão bem-sucedida!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Falha na conexão!");
+        }
+    }
 }
