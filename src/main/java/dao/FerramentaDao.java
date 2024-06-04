868f00f6199f18ee3c5904bd7d0667905a753d43
Eduardocoelh0 <eduardoalfredofaculdade@gmail.com>
2024-06-04 08:47:55 -0300

    Dox: correção Ferramenta

--- src/main/java/dao/FerramentaDao.java
+++ src/main/java/dao/FerramentaDao.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package dao;
+public class Ferramenta {
+    private int id;
+    private String nome;
+    private String marca;
+    private double custoAquisicao;
 
-/**
- *
- * @author 1072419300
- */
-public class FerramentaDao {
-    
+    public Ferramenta(int id, String nome, String marca, double custoAquisicao) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.custoAquisicao = custoAquisicao;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCustoAquisicao() {
+        return custoAquisicao;
+    }
+
+    public void setCustoAquisicao(double custoAquisicao) {
+        this.custoAquisicao = custoAquisicao;
+    }
+
+    @Override
+    public String toString() {
+        return "Ferramenta [id=" + id + ", nome=" + nome + ", marca=" + marca + ", custoAquisicao=" + custoAquisicao + "]";
+    }
 }
