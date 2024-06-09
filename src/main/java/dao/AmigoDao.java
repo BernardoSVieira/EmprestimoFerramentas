86e41f79b34fe6365cb0316c773074b4fc9a02c2
GuilhermeSchveitzer <guischveitzer@gmail.com>
2024-06-08 21:51:02 -0300

    Fix: AmigoDao arrumei os erros e o que estava desconexo

--- src/main/java/dao/AmigoDao.java
+++ src/main/java/dao/AmigoDao.java
@@ -1,65 +1,92 @@
 package dao;
 
-import dao.AmigoDao.Amigo;
+import java.util.ArrayList;
 import java.util.List;
 import javax.lang.model.SourceVersion;
 
 public class AmigoDao {
-    public class Amigo {
-       
-    private static int contador = 1;
-    private final int id;
-    private String nome;
-    private String email;
 
-    public AmigoDao(String nome, String email) {
-        this.id = contador++;
-        this.nome = nome;
-        this.email = email;
+    public static class Amigo {
+        private static int contador = 1;
+        private final int id;
+        private String nome;
+        private String email;
+
+        public Amigo(String nome, String email) {
+            this.id = contador++;
+            this.nome = nome;
+            this.email = email;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.latest();
+        }
     }
 
-    public int getId() {
-        return id;
+    public interface AmigoDAO {
+        List<Amigo> listarTodos();
+        Amigo buscarPorId(int id);
+        void inserir(Amigo amigo);
+        void atualizar(Amigo amigo);
+        void deletar(int id);
     }
 
-    public String getNome() {
-        return nome;
-    }
+    public static class AmigoDAOImpl implements AmigoDAO {
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+        private List<Amigo> amigos = new ArrayList<>();
 
-    public String getEmail() {
-        return email;
-    }
+        @Override
+        public List<Amigo> listarTodos() {
+            return new ArrayList<>(amigos);
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+        @Override
+        public Amigo buscarPorId(int id) {
+            for (Amigo amigo : amigos) {
+                if (amigo.getId() == id) {
+                    return amigo;
+                }
+            }
+            return null;
+        }
 
-    public SourceVersion getSupportedSourceVersion() {
-        return SourceVersion.latest();
-    }
+        @Override
+        public void inserir(Amigo amigo) {
+            amigos.add(amigo);
+        }
 
-    interface AmigoDAO {
-    List<Amigo> listarTodos();
-    Amigo buscarPorId(int id);
-    void inserir(Amigo amigo);
-    void atualizar(Amigo amigo);
-    void deletar(int id);
-}
+        @Override
+        public void atualizar(Amigo amigo) {
+            Amigo existente = buscarPorId(amigo.getId());
+            if (existente != null) {
+                existente.setNome(amigo.getNome());
+                existente.setEmail(amigo.getEmail());
+            }
+        }
 
-    class AmigoDAOImpl extends AmigoDAO {
-
-    private Iterable<Amigo> amigos;
-    
-    public AmigoDAOImpl(String nome, String email) {
-        super(nome, email);
-    }
-
-    public List<Amigo> listarTodos() {
-        return (List<Amigo>) amigos;
+        @Override
+        public void deletar(int id) {
+            amigos.removeIf(amigo -> amigo.getId() == id);
         }
     }
 }
