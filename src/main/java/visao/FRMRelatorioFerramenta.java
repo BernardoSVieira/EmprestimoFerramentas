99dc59917481b05613a51c6e409f5737827fc3d4
Eduardo Alfredo Coelho <166423906+Eduardocoelh0@users.noreply.github.com>
2024-06-10 17:38:38 -0300

    Feat: nomeando e adição dos botoes da tela FRMRelatorioFerramenta

--- src/main/java/visao/FRMRelatorioFerramenta.java
+++ src/main/java/visao/FRMRelatorioFerramenta.java
@@ -20,8 +20,11 @@ public class FRMRelatorioFerramenta extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTabela = new javax.swing.JTable();
-        jLPreçoTotalFerramentas = new javax.swing.JLabel();
         jLSomaTodosOsCustos = new javax.swing.JLabel();
+        jBCalcular = new javax.swing.JButton();
+        jBCancelar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jBConfirmar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Relatório de Ferramentas");
@@ -46,23 +49,38 @@ public class FRMRelatorioFerramenta extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jTabela);
 
-        jLPreçoTotalFerramentas.setText("Preço total das ferramentas:");
+        jBCalcular.setText("Calcular");
+
+        jBCancelar.setText("Cancelar");
+
+        jButton1.setText("Atualizar");
+
+        jBConfirmar.setText("Confirmar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 599, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jBCalcular, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(123, 123, 123)
-                        .addComponent(jLPreçoTotalFerramentas)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLSomaTodosOsCustos, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 559, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(119, 119, 119)
+                        .addComponent(jLSomaTodosOsCustos, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jBConfirmar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jBCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(136, 136, 136))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -71,9 +89,15 @@ public class FRMRelatorioFerramenta extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLPreçoTotalFerramentas)
-                    .addComponent(jLSomaTodosOsCustos))
-                .addContainerGap(30, Short.MAX_VALUE))
+                    .addComponent(jBCalcular)
+                    .addComponent(jBCancelar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLSomaTodosOsCustos)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jBConfirmar))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
@@ -115,7 +139,10 @@ public class FRMRelatorioFerramenta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLPreçoTotalFerramentas;
+    private javax.swing.JButton jBCalcular;
+    private javax.swing.JButton jBCancelar;
+    private javax.swing.JButton jBConfirmar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLSomaTodosOsCustos;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTabela;
